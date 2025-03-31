740ce40e2e17a0a66a1bdcef215dca8b39db1d00
Christian0119 <ishtian2006@gmail.com>
2025-03-31 20:17:07 +0800

    colorPreference.js

---
+++ colorPreference.js
@@ -0,0 +1,12 @@
+let favoriteColors = [];
+
+
+for (let i = 0; i < 3; i++) {
+
+    let color = prompt(Enter color ${i + 1}:);
+
+    favoriteColors.push(color);
+
+    console.log("Updated colors:", favoriteColors);
+
+}
